0f505b9f5cc687417e952153a48b48d8efc7a96f
Setiadi Pratama <45945725+idaitesamatarp@users.noreply.github.com>
2020-01-19 23:26:22 +0700

    Baru

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -6,7 +6,8 @@ public class FollowTarget : MonoBehaviour
 {
     public Transform player;
     public Transform Bg1;
-    public Transform Bg2;
+    // public Transform Bg2;
+    // public Transform Bg3;
     
     // Use this for initialization
     void Start()
@@ -17,12 +18,13 @@ public class FollowTarget : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (player.position.x != transform.position.x && player.position.x > 0 && player.position.x < 12f)
+        if (player.position.x != transform.position.x && player.position.x > 0 && player.position.x < 20f)
         {
             transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, transform.position.y, transform.position.z), 0.1f);
         }
         Bg1.transform.position = new Vector2(transform.position.x * 1.0f, Bg1.transform.position.y);
-        Bg2.transform.position = new Vector2(transform.position.x * 0.8f, Bg2.transform.position.y);        
+        // Bg2.transform.position = new Vector2(transform.position.x * 1.0f, Bg2.transform.position.y);
+        // Bg3.transform.position = new Vector2(transform.position.x * 0.6f, Bg3.transform.position.y);
     }
 
 }
