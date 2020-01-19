af5501e4233ab28fd7af516e94ecfa521ce6595d
Setiadi Pratama <45945725+idaitesamatarp@users.noreply.github.com>
2020-01-19 23:52:10 +0700

    New

--- Assets/Scripts/FollowTarget.cs
+++ Assets/Scripts/FollowTarget.cs
@@ -6,15 +6,7 @@ public class FollowTarget : MonoBehaviour
 {
     public Transform player;
     public Transform Bg1;
-    // public Transform Bg2;
-    // public Transform Bg3;
-    
-    // Use this for initialization
-    void Start()
-    {
- 
-    }
- 
+
     // Update is called once per frame
     void Update()
     {
@@ -23,8 +15,6 @@ public class FollowTarget : MonoBehaviour
             transform.position = Vector3.Lerp(transform.position, new Vector3(player.position.x, transform.position.y, transform.position.z), 0.1f);
         }
         Bg1.transform.position = new Vector2(transform.position.x * 1.0f, Bg1.transform.position.y);
-        // Bg2.transform.position = new Vector2(transform.position.x * 1.0f, Bg2.transform.position.y);
-        // Bg3.transform.position = new Vector2(transform.position.x * 0.6f, Bg3.transform.position.y);
     }
 
 }
---
+++ Assets/Scripts/KillPlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KillPlayer : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.transform.CompareTag("Player"))
+        {
+            if(Data.nyawa == 1)
+            {
+                Data.nyawa--;
+                int scene = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(scene, LoadSceneMode.Single);
+                Time.timeScale = 1;
+            } else
+            {
+                Data.nyawa--;
+                Data.score = 0;
+                int scene = SceneManager.GetActiveScene().buildIndex;
+                SceneManager.LoadScene(scene, LoadSceneMode.Single);
+                Time.timeScale = 1;
+            }
+           
+        }
+    }
+}
