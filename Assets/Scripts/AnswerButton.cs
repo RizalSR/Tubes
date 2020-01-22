f466f51c10aadee255a283854d25a3eb55301b8f
RizalSR <rizalsaidramadhan98@gmail.com>
2020-01-22 18:45:59 +0700

    QUIZ

---
+++ Assets/Scripts/AnswerButton.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerButton : MonoBehaviour
+{
+    public Text answerText;
+    private AnswerData answerData;
+    private GameController gameController;
+
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    public void AnswerSetup(AnswerData data)
+    {
+        answerData = data;
+        answerText.text = answerData.answerText;
+    }
+
+    public void HandleClick()
+    {
+        gameController.AnswerButtonClicked(answerData.isCorrect);
+    }
+}
---
+++ Assets/Scripts/RoundData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[System.Serializable]
+public class RoundData
+{
+   public String name;
+   public int timeLimitInSeconds;
+   public int pointAddedForCorrextAnswer;
+   public QuestionData[] question; 
+}
