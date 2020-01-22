165e4e2f690b9025f878e0e040bea3d96f798df3
Rohalyasals <rohalya.salsabila@gmail.com>
2020-01-23 01:59:55 +0700

    score gameover

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameOverController : MonoBehaviour
+{
+    public Text txScore;
+    Text txSelamat;
+    int score;
+    int total;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = Data.score;
+        total = score;
+        txScore.text = total.ToString();
+    }
+
+    public void Menu(){
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Exit(){
+        Application.Quit();
+    }
+}
